301db37638282a54a0e118cdb00b5b0dbef9ab63
Carlos Cardoso <carloshr.cardoso@outlook.com>
2024-08-27 14:32:17 -0300

    initial commit

---
+++ src/main/java/com/example/extrator_pdf/ExtratorPdfApplication.java
@@ -0,0 +1,13 @@
+package com.example.extrator_pdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtratorPdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtratorPdfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/extrator_pdf/ExtratorPdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.extrator_pdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtratorPdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
